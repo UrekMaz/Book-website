e32d38bc483680b1c76f88897e1e4411c71a9e03
UrekMaz <dcmaureenmiranda@gmail.com>
2024-07-16 18:29:32 +0530

    x

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask
-
-
-
+from flask import Flask,render_template
+import pickle
+books_df=pickle.load(open('books.pkl','rb'))
 app=Flask(__name__)
 
 
 
 @app.route('/')
 def index():
-    return "hello"
+    return render_template('index.html',result=)
 
 if __name__ == '__main__':
     app.run(debug=True)
